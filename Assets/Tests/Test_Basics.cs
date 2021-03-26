aaab188b96a898088dbf9b03045fdcb7eebd7a26
Reavenk <Reavenk@gmail.com>
2021-03-26 18:26:15 -0500

    Simple unit tests moved to its own directly
    Separate out tests so they won't clutter the same space as more are added.

--- Assets/Tests/Test_Basics.cs
+++ Assets/Tests/Test_Basics.cs
@@ -11,79 +11,79 @@ namespace Tests
         [Test]
         public void Test_BasicsSimple_001()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple001.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple001.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_002()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple002.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple002.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_003()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple003.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple003.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_004()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple004.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple004.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_005()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple005.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple005.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_006()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple006.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple006.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_007()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple007.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple007.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_008()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple008.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple008.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_009()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple009.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple009.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_010()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple010.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple010.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_011()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple011.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple011.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_012()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple012.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple012.synsyn");
         }
 
         [Test]
         public void Test_BasicsSimple_013()
         {
-            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Vali_Simple013.synsyn");
+            UnitTest_Utils.PerformContainedTest("Assets/SynthScripts/Simple/Vali_Simple013.synsyn");
         }
     }
 }
